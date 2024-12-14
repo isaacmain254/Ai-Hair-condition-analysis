e5250410349131e78549e2c5dc8f5dbe0e3072c9
isaacmain254 <isaacmain72@gmail.com>
2024-12-14 09:23:51 +0300

    add Vercel configuration and update Vite build options for improved routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
+  build: {
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
   server: {
     port: 3000,
   },
